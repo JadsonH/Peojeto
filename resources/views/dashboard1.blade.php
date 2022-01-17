e8b5176a877e952105a398f437746252eace6ea5
JadsonH <jadsonsantoscaico2021@gmail.com>
2022-01-17 18:14:19 -0300

    Adicionando...

--- resources/views/dashboard1.blade.php
+++ resources/views/dashboard1.blade.php
@@ -9,7 +9,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                   <div id="copy-area">
-                   @endforeach
                    @can('admin')
                    <form action="{{route('salvar1')}}" method="POST">
                    @csrf
